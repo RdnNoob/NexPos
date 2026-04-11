481299ea67507302348800eeed90301d61adbfbb
Raden Muhammad Ichsan <52437658+RdnNoob@users.noreply.github.com>
2026-04-12 01:48:57 +0700

    fix: hapus TypeAdapterFactory, cukup setLenient() + perbaiki CancellationException

--- core/src/main/java/com/nexpos/core/di/NetworkModule.kt
+++ core/src/main/java/com/nexpos/core/di/NetworkModule.kt
@@ -1,13 +1,6 @@
 package com.nexpos.core.di
 
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.google.gson.TypeAdapter
-import com.google.gson.TypeAdapterFactory
-import com.google.gson.reflect.TypeToken
-import com.google.gson.stream.JsonReader
-import com.google.gson.stream.JsonToken
-import com.google.gson.stream.JsonWriter
 import com.nexpos.core.BuildConfig
 import com.nexpos.core.data.api.NexPosApi
 import dagger.Module
@@ -20,43 +13,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
-
-private class FlexibleNumberTypeAdapterFactory : TypeAdapterFactory {
-    @Suppress("UNCHECKED_CAST")
-    override fun <T> create(gson: Gson, type: TypeToken<T>): TypeAdapter<T>? {
-        return when (type.rawType) {
-            Int::class.java, java.lang.Integer::class.java -> object : TypeAdapter<Int>() {
-                override fun write(out: JsonWriter, value: Int?) {
-                    if (value == null) out.nullValue() else out.value(value)
-                }
-                override fun read(input: JsonReader): Int {
-                    return when (input.peek()) {
-                        JsonToken.STRING -> input.nextString().toIntOrNull() ?: 0
-                        JsonToken.NUMBER -> input.nextInt()
-                        JsonToken.NULL -> { input.nextNull(); 0 }
-                        else -> { input.skipValue(); 0 }
-                    }
-                }
-            } as TypeAdapter<T>
-
-            Long::class.java, java.lang.Long::class.java -> object : TypeAdapter<Long>() {
-                override fun write(out: JsonWriter, value: Long?) {
-                    if (value == null) out.nullValue() else out.value(value)
-                }
-                override fun read(input: JsonReader): Long {
-                    return when (input.peek()) {
-                        JsonToken.STRING -> input.nextString().toLongOrNull() ?: 0L
-                        JsonToken.NUMBER -> input.nextLong()
-                        JsonToken.NULL -> { input.nextNull(); 0L }
-                        else -> { input.skipValue(); 0L }
-                    }
-                }
-            } as TypeAdapter<T>
-
-            else -> null
-        }
-    }
-}
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -90,7 +46,7 @@ object NetworkModule {
         }
         val gson = GsonBuilder()
             .setLenient()
-            .registerTypeAdapterFactory(FlexibleNumberTypeAdapterFactory())
+            .serializeNulls()
             .create()
         return Retrofit.Builder()
             .baseUrl(baseUrl)
